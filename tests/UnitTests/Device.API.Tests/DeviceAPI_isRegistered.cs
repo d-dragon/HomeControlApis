d37e1a9070b5444c20fb18400e29ce765249b348
Duy Phan <phanduyt291@gmail.com>
2018-12-18 23:50:20 +0700

    Add xUnit Test project for Device.API

---
+++ tests/UnitTests/Device.API.Tests/DeviceAPI_isRegistered.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using Device.API;
+
+namespace Devices.Tests
+{
+    public class DeviceAPI_isRegistered
+    {
+        
+        public DeviceAPI_isRegistered()
+        {
+            
+        }
+        [Fact]
+        public void CheckDeviceisExisted()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+            Assert.False(false);
+        }
+    }
+}
